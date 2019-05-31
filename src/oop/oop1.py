4c672875f57eeec65d7deb085d9e40e0d5cdd075
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-31 13:58:23 -0400

    oop1 tasks completed.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -23,26 +23,32 @@ class Vehicle:
     def __init__(self):
         pass
 
-class FlightVehicle:
+class FlightVehicle(Vehicle):
     def __init__(self):
         super().__init__()
+        pass
 
-class Starship:
+class Starship(FlightVehicle):
     def __init__(self):
-        super().__init__()        
+        super().__init__()
+        pass        
 
 class Airplane(FlightVehicle):
     def __init__(self):
         super().__init__()
+        pass
 
 class GroundVehicle(Vehicle):
     def __init__(self):
         super().__init__()
+        pass
 
 class Car(GroundVehicle):
     def __init__(self):
         super().__init__()
+        pass
 
 class Motorcycle(GroundVehicle):
     def __init__(self):
         super().__init__()
+        pass
